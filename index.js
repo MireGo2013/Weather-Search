9cc5fc8803852c5963486cf6516a340db265a1f4
Vladilen Sobol <sobol.v92@gmail.com>
2021-06-21 17:56:06 +0300

    created heroku

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const weatherRequest = require('./requests/weather.request.js')
 
+const PORT = process.env.PORT || 90
 const app = express()
 
 app.set('view engine', 'ejs')
@@ -19,7 +20,7 @@ app.post('/', async (req, res) => {
 	res.render('index', { weather, error })
 })
 
-app.listen(3000, () => {
-	console.log('Server has started on port 3000...')
+app.listen(PORT, () => {
+	console.log('Server has been started on port...')
 })
 
